1bc4d11a91589a647e7d5861590f23e783652277
ezaman <ehsanmzaman@gmail.com>
2016-05-15 18:40:55 -0400

    done

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -9,10 +9,41 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+- (BOOL) stringIsPalindrome:(NSString *)string {
+    
+    NSArray *punctuations = @[@".", @"!", @",", @"?", @";", @":"];
+    NSString *withOutPunctuation = [string copy];
+    
+    for (NSUInteger i = 0; i < [punctuations count]; i++) {
+        
+        NSString *punctuation = punctuations[i];
+        withOutPunctuation = [withOutPunctuation
+    stringByReplacingOccurrencesOfString:punctuation withString:@""];
+    }
+    
+    NSString *spaceless = [withOutPunctuation
+stringByReplacingOccurrencesOfString:@" " withString:@""];
+    
+    NSString *lowercase = [spaceless lowercaseString];
+    NSString *reverse = [self stringByReversingString:lowercase];
+    BOOL stringIsEqualToReverse = [lowercase isEqualToString: reverse];
+    
+    return stringIsEqualToReverse;
+
+}
+    
+- (NSString *)stringByReversingString:(NSString *)string{
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%c", c];
+    }
+    
+    return result;
+
+}
+
 
 @end
